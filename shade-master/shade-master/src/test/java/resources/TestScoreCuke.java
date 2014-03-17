f6130011e43987a220cbe6d07a75e7a181094720
bcm32 <bcm3223@gmail.com>
2014-03-17 15:29:08 -0400

    Removed commented out code

--- shade-master/shade-master/src/test/java/resources/TestScoreCuke.java
+++ shade-master/shade-master/src/test/java/resources/TestScoreCuke.java
@@ -17,12 +17,8 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @SuppressStaticInitializationFor({"com.shade.entities.mushroom.Mushroom"})
-//@RunWith(PowerMockRunner.class)
 public class TestScoreCuke {
-		//Player p;
-		//Linkable m1;
-		//Linkable m2;
-		//Linkable m3;
+		
 		Mushroom m1, m2, m3, gm;
 		CounterControl cc;
 		ScoreControl sc;
@@ -31,25 +27,11 @@ public class TestScoreCuke {
 		
 		@Given("there are (\\d+) normal mushrooms")
 		public void number_of_mushrooms(int shrooms) throws SlickException{
-			//p = Mockito.mock(Player.class);
 			
-		  /*try {
-				m1 = Mockito.mock(Mushroom.class);
-				m2 = Mockito.mock(Mushroom.class);
-				m3 = Mockito.mock(Mushroom.class);
-		} catch (Throwable t) {
-		    System.out.println("Failure during static initialization " + t );
-		  }*/
 			m1 = Mockito.mock(Mushroom.class);
 			m2 = Mockito.mock(Mushroom.class);
 			m3 = Mockito.mock(Mushroom.class);
-			/*MushroomFactory x = Mockito.mock(MushroomFactory.class);
-			m1 = new Mushroom(0,0,null, x);
-			m2 = new Mushroom(0,0,null, x);
-			m3 = new Mushroom(0,0,null, x);*/
-			//p.attach(m1);
-			//p.attach(m2);
-			//p.attach(m3);
+			
 			cc = new CounterControl(0, 0, null, null, null);
 			sc = new ScoreControl(0,0,null);
 			cc.register(sc);
@@ -60,9 +42,6 @@ public class TestScoreCuke {
 		@Given("there is 1 gold mushroom")
 		public void number_of_gold_mushrooms() throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException{
 			gm = Mockito.mock(Mushroom.class);
-			/*Field privateValueField = Mushroom.class.getDeclaredField("type");
-			privateValueField.setAccessible(true);
-			privateValueField.set(gm, "RARE");*/
 			Mockito.stub(gm.isGolden()).toReturn(true);
 			cc = new CounterControl(0, 0, null, null, null);
 			sc = new ScoreControl(0,0,null);
